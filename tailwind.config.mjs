f6872b86c3f14379414ae7b5489e7ab96bae62b8
Manglam <manglamdubey11@gmail.com>
2025-01-16 11:52:30 +0530

    about me section completed

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,13 +10,17 @@ export default {
   theme: {
     extend: {
       colors: {
-        lightHover: "#fcf4ff",
+        lightHover: "#f1e6f2",
         darkHover: "#2a004a",
         darkTheme: "#11001f",
       },
       fontFamily: {
         Outfit: ["Outfit", "sans-serif"],
         Ovo: ["Ovo", "serif"],
+      },
+      boxShadow: {
+        black: "4px 4px 0 #000",
+        white: "4px 4px 0 #fff",
       },
     },
   },
